35bb17e3dd5952f58afd8aa857260a4d77f8b7e1
world-start <23fn40@gmail.com>
2021-07-31 06:34:57 +0000

    [Finish]bookers1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,8 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
+
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  post 'books/new' => 'books#create'
+  post 'books' => 'books#create'
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as:'book'
   get 'books/:id/edit' => 'books#edit', as:'edit_book'
